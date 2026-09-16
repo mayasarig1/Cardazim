b59c9847b86f8456c97d07b7fdde623b5f97e5dd
Maya Sarig <maya.sarig43@gmail.com>
2026-09-16 15:15:19 +0300

    redo 1

--- Networking/server.py
+++ Networking/server.py
@@ -7,16 +7,17 @@ lock = threading.Lock()
 
 # handles data from each user seperately using "lock"
 def handle_client(conn, addr):
-    with lock:
-        data_length_bytes = conn.recv(4)
-        data_length = int.from_bytes(data_length_bytes, 'little')
-        from_client = ''
-        while True:
-            data = conn.recv(data_length)
-            if not data: break
-            from_client += data.decode('utf8')
-            print (f'From client: {from_client}')
-        conn.close()
+    data_length_bytes = conn.recv(4)
+    data_length = int.from_bytes(data_length_bytes, 'little')
+    from_client = bytes(''.encode())
+    while True:
+        data = conn.recv(data_length)
+        if not data: 
+            break
+        from_client += data
+        with lock:
+            print (f'From client: {from_client.decode('utf8')}')
+    conn.close()
 
 
 def run_server(ip, port):
@@ -27,7 +28,6 @@ def run_server(ip, port):
         conn, addr = serv.accept()
         t = threading.Thread(target=handle_client, args=(conn, addr))
         t.start()
-        t.join()
 
 if __name__ == "__main__":
     ip = sys.argv[1]
